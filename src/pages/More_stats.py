2d85fa23560c0d4e4af27e3ac31797f289adbe88
Tang <138358213+tcw1470@users.noreply.github.com>
2025-04-02 15:43:22 -0700

    Create More_stats.py

---
+++ src/pages/More_stats.py
@@ -0,0 +1,53 @@
+import sys, os
+
+# Get the parent directory
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+import utils
+
+from importlib import reload
+reload( utils )
+
+
+# ============================== main =============================
+
+utils.st.title( "Population around the world" )         
+
+w_df = utils.pd.read_csv( utils.Path(f"{utils.root_path}/data/ne_110m_admin_0_countries.csv") )
+w_df = w_df.rename( columns= {'LABEL_X':'Longitude', 'LABEL_Y':'Latitude' } )
+
+utils.st.header( "Query" )         
+world_sub = utils.filter_dataframe( w_df[[ 'SOVEREIGNT','TYPE','GDP_YEAR','POP_EST','Longitude','Latitude']].copy(), 'Add filters?' )                    
+g=world_sub.iloc[:,:-1]
+g=utils.pd.DataFrame(g)
+utils.st.dataframe( g )      
+utils.st.write( w_df.sample(3).transpose()  )         
+
+
+utils.st.header( "Population vs GDP" )           
+fig=utils.px.scatter( w_df, y='POP_EST', x='GDP_YEAR', color='SOVEREIGNT', title='GDP vs population')  #"layout.hovermode='color'"  )                    
+fig.show()
+utils.st.plotly_chart(fig)
+
+try:
+  fig = utils.px.scatter( w_df, x="POP_EST", y="GDP_YEAR", size="POP_EST", color="LABELRANK",
+                 hover_name="SOVEREIGNT", log_x=True, size_max=60, title = 'GDP vs population')
+  fig.show()
+  utils.st.plotly_chart(fig)
+except:
+  pass
+  
+
+csv = utils.convert_df( world_sub )    
+utils.st.download_button(
+   "Press to download CSV containing geocoordinates of select countries",
+   csv,
+   "world_geo.csv",
+   "text/csv",
+   key='download-global-geo-csv'
+) 
+
+utils.st.print_footer()
