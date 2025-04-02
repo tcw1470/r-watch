b63024ff10dc05b92c7b0fd2bce01870162b0505
Tang <138358213+tcw1470@users.noreply.github.com>
2025-04-02 16:17:46 -0700

    Update More_stats.py

--- src/pages/More_stats.py
+++ src/pages/More_stats.py
@@ -19,14 +19,6 @@ utils.st.title( "Population around the world" )
 w_df = utils.pd.read_csv( utils.Path(f"{utils.root_path}/data/ne_110m_admin_0_countries.csv") )
 w_df = w_df.rename( columns= {'LABEL_X':'Longitude', 'LABEL_Y':'Latitude' } )
 
-utils.st.header( "Query" )         
-world_sub = utils.filter_dataframe( w_df[[ 'SOVEREIGNT','TYPE','GDP_YEAR','POP_EST','Longitude','Latitude']].copy(), 'Add filters?' )                    
-g=world_sub.iloc[:,:-1]
-g=utils.pd.DataFrame(g)
-utils.st.dataframe( g )      
-utils.st.write( w_df.sample(3).transpose()  )         
-
-
 utils.st.header( "Population vs GDP" )           
 fig=utils.px.scatter( w_df, y='POP_EST', x='GDP_YEAR', color='SOVEREIGNT', title='GDP vs population')  #"layout.hovermode='color'"  )                    
 fig.show()
@@ -40,6 +32,22 @@ try:
 except:
   pass
   
+utils.st.write( "Filter the following dataframe to help answer your queries" )         
+world_sub = utils.filter_dataframe( w_df[[ 'SOVEREIGNT','TYPE','GDP_YEAR','POP_EST','Longitude','Latitude']].copy(), 'Add filters?' )                    
+g=world_sub.iloc[:,:-1]
+g=utils.pd.DataFrame(g)
+
+utils.st.write( 'Filtered subset:' )
+utils.st.dataframe( g )      
+csv2 = utils.convert_df( g )    
+utils.st.download_button(
+   "Press to download filtered subset",
+   csv2,
+   "subset_geo.csv",
+   "text/csv",
+   key='download-subset'
+) 
+
 
 csv = utils.convert_df( world_sub )    
 utils.st.download_button(
