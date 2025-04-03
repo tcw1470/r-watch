23de281673d5e0756725aeceb2eb38a1b94aa694
Tang <138358213+tcw1470@users.noreply.github.com>
2025-04-02 17:32:24 -0700

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -31,46 +31,15 @@ def main():
   intro_markdown = utils.read_markdown_file( utils.Path( gparent_dir, "README.md") )
   utils.st.markdown(intro_markdown, unsafe_allow_html=True)
 
-  if 0:    
-    from pip.operations import freeze    
-    modules = list(
-        map(lambda x: x.split('=='), freeze.freeze(local_only=True))
-    )  
-  elif 0:    
-    import pip
-    modules = []
-    for i in pip.utils.get_installed_distributions():
-        modules.append((i.key, i.version))
-  else:
-    from pip._internal.utils.misc import get_installed_distributions
-    p = get_installed_distributions()
-    pprint.pprint(p)
-    utils.st.write( p ) 
+  import pkg_resources
 
-    #from pkgutil import iter_modules
-    #utils.st.write([p.name for p in iter_modules()])
-
-    try:
-      from pip._internal.operations import list
-    except Exception as e:
-      print( 'cannot load list', e ) 
-      
-    try: 
-      from pip._internal.operations import freeze      
-      utils.st.write( '1' ) 
-    except ImportError: # pip < 10.0
-      from pip.operations import freeze
-      utils.st.write( '2' ) 
-    
-    pkgs = freeze.freeze()
-    
-    s = '''
-    <details> 
-      <summary>Python packages used by this app </summary>
-    '''
-    # s = ' '.join(pkgs) # + '</details>'
-    print( pkgs )
-     
+  # Get the working set (all installed packages)
+  working_set = pkg_resources.working_set
+  
+  # Iterate through the distributions (packages)
+  for distribution in working_set:
+      print(f'{distribution.project_name}=={distribution.version}')
+                                            
 
 if __name__ == '__main__':
   main()
